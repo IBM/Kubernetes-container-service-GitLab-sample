ec0464bcfe0e4f5c23a0da7a4047555fbe42b47e
Ryan Moe <ryan.moe@gmail.com>
2017-03-31 09:36:52 -0700

    CI changes

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 function bluemix_auth() {
 	echo "Authenticating with Bluemix"
-	bx login -u "$BX_USER" -p "$BX_PASS" -c 6aae3a1eef7c0d9f0d6ae1e69e949b2a > /dev/null
+	bx login -a https://api.ng.bluemix.net-u "$BX_USER" -p "$BX_PASS" -c 6aae3a1eef7c0d9f0d6ae1e69e949b2a > /dev/null
 	yes | bx plugin install container-registry -r bluemix
 	bx cr login
 	bx cs init
@@ -40,7 +40,7 @@ function run_tests() {
 
 	sleep 60
 	IP=$(kubectl get nodes | awk 'NR>1 {print $1}')
-	curl -f --verbose http://$IP:30080 -o /dev/null || exit_tests 1
+	curl -f http://$IP:30080 -o /dev/null || exit_tests 1
 
 
 	exit_tests 0
