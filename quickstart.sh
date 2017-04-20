5517833e72364beafda540079739ff30c0a06e44
Paul Czarkowski <username.taken@gmail.com>
2017-04-20 16:35:54 -0500

    update journey to include basic docker instruction
    This helps make the journey a little bit more accessible by updating
    it to use only official community images and to also show deploying
    locally via `docker` or `docker-compose` as well as to Bluemix Container
    Service.

--- quickstart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl create -f local-volumes.yaml
-kubectl create -f postgres.yaml
-sleep 5s
-kubectl create -f redis.yaml
-sleep 5s
-kubectl create -f gitlab.yaml
-kubectl get nodes
-kubectl get svc gitlab
