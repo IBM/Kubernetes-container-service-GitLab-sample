216799e9f43639713c56de91df1ed18b9e340c57
Matt Langbehn <matthew.langbehn@gmail.com>
2017-08-18 16:03:02 -0500

    Add linters to Travis CI configuration
    Previously, Travis CI installed software (some of which was not needed),
    and then ran `/bin/true`.
    
    Now, the Travis CI configuration has been cleaned up, and runs
    shellcheck and yamllint. In the process, several .sh and .yml files had
    to be updated so that they would pass linting.
    
    Related-Issue: IBM/Kubernetes-container-service-GitLab-sample#26
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- scripts/bx_login.sh
+++ scripts/bx_login.sh
@@ -1,35 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z $CF_ORG ]; then
+if [[ -z "$CF_ORG" ]]; then
   CF_ORG="$BLUEMIX_ORG"
 fi
-if [ -z $CF_SPACE ]; then
+if [[ -z "$CF_SPACE" ]]; then
   CF_SPACE="$BLUEMIX_SPACE"
 fi
 
 
-if ([ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]) && ([ -z "$API_KEY"]); then
+if ([ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]) && ([ -z "$API_KEY" ]); then
   echo "Define all required environment variables and re-run the stage."
   exit 1
 fi
 
 echo "bx login -a $CF_TARGET_URL"
 
-if [ -z "$API_KEY"]; then
-  bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+if [[ -z "$API_KEY" ]]; then
+  bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE" \
+      || echo "Failed to authenticate to Bluemix"
 else
-  bx login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+  bx login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE" \
+      || echo "Failed to authenticate to Bluemix"
 fi
 
-if [ $? -ne 0 ]; then
-  echo "Failed to authenticate to Bluemix"
-  exit 1
-fi
-
-# Init container clusters
 echo "bx cs init"
-bx cs init
-if [ $? -ne 0 ]; then
+
+if ! bx cs init ; then
   echo "Failed to initialize to Bluemix Container Service"
   exit 1
 fi
