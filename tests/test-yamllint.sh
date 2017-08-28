7045d1a99be90ac9865115ddb6434480b6ce6e78
Matt Langbehn <matthew.langbehn@gmail.com>
2017-08-28 17:36:11 -0500

    Support standard YAML file extensions
    Previously, we were not linting files with the .yaml file extension.
    Now, we are.
    
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- tests/test-yamllint.sh
+++ tests/test-yamllint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if find . -name '*.yml' -print0 | xargs -n1 -0 yamllint -c .yamllint.yml; then
+if find . \( -name '*.yml' -o -name '*.yaml' \) -print0 | xargs -n1 -0 yamllint -c .yamllint.yml; then
     echo -e "\033[0;32mYAML linting passed!\033[0m"
 else
     echo -e >&2 "\033[0;31mYAML linting failed!\033[0m"
